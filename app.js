f3df08520411cfa364408b7123ba45638777e88f
dn00z <hgdnimantha@gmail.com>
2022-07-27 20:33:18 +0530

    added data get end point

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ mongoose.connect(process.env.MONGODB_CONNECT_URI)
 	.then(() => console.log('Connected to MongoDB...'))
 	.catch(err => console.error('Could not connect to MongoDB...', err));
 
+//get Routes
+const alerts = require('./routes/alerts');
+const crypto = require('./routes/crypto');
+
 //Set Routers
-app.use('/alerts', require('./routes/alerts'));
+app.use('/alerts', alerts);
+app.use('/crypto', crypto);
 
 exports.app = app;

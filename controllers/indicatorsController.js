0e7ee34ff96cfeefa2b44bebc7b92436e048e31d
sanduninduwara <sanduninduwara2@gmail.com>
2022-10-03 13:45:57 +0530

    add all TA helper functions

--- controllers/indicatorsController.js
+++ controllers/indicatorsController.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable */
 const {
 	sma_inc,
 	ema_inc,
@@ -12,33 +12,57 @@ const {
 	obv_inc 
 } = require('../helper/indicators');
   
-const { Alert } = require('../models/alert');
 const { User } = require('../models/user');
+const { getCandleModelForCollection } = require('../models/candleStick');
 const asyncHandler = require('express-async-handler');
 
 
 
-//Examples assume you have some price data like this:
-//Data order is from oldest to newset (index 0 is the oldest)
-var open  = [4,5,5,5,4,4,4,6,6,6];
-var high  = [9,7,8,7,8,8,7,7,8,7];
-var low   = [1,2,3,3,2,1,2,2,2,3];
-var close = [4,5,6,6,6,5,5,5,6,4];
-var volume = [123,232,212,232,111,232,212,321,232,321];
-
 const getTAData =asyncHandler( async (req,res) => {
-	const klinedata = await obv_inc(close,volume);
-	// const bbannd= await bbands_inc(close);
-	// const wma = await wma_inc(close);
-	// klinedata = await ema_inc(klinedata);
-	// klinedata = markers_inc(klinedata);
-	// klinedata = await rsi_inc(klinedata);
-	// klinedata = await macd_inc(klinedata);
-	await console.log(klinedata)
-	res.status(200).json(klinedata);
+	
+	//get recent 10 cs data
+	const dbdata= await getCandleModelForCollection('binance_BNBUSDT_15m').find().sort({ _id: -1 }).limit(10);
+
+	//re-format data for TA
+	var dataobj=structuredData(dbdata);
+	console.log(dataobj);
+
+	//get calculated data
+	const ema = await ema_inc(dataobj.close_price);
+
+
+	res.status(200).json(ema);
 	
 });
 
+
+
+function structuredData(datalist){
+	var dataobj={
+		_id:[],
+		open_time: [],
+		close_time: [],
+		open_price: [],
+		close_price: [],
+		high_price: [],
+		low_price: [],
+		volume: [],
+		ta:[]
+	};
+	datalist.forEach(element => {
+		dataobj.open_time.push(element.open_time);
+		dataobj.close_time.push(element.close_time);
+		dataobj._id.push(element._id);
+		dataobj.open_price.push(element.open_price);
+		dataobj.close_price.push(element.close_price);
+		dataobj.high_price.push(element.high_price);
+		dataobj.low_price.push(element.low_price);
+		dataobj.volume.push(element.volume);
+		
+	});
+
+	return dataobj;
+}
 
 
 module.exports = {
--- helper/indicators.js
+++ helper/indicators.js
@@ -3,7 +3,10 @@ const { promisify } = require('util');
 
 
 //Promisify Functions
+
+//SMA
 const sma_async = promisify(tulind.indicators.sma.indicator);
+//EMA
 const ema_async = promisify(tulind.indicators.ema.indicator);
 //MA
 //BBANDS
@@ -11,9 +14,9 @@ const bbands_async = promisify(tulind.indicators.bbands.indicator);
 //WMA
 const wma_async = promisify(tulind.indicators.wma.indicator);
 
-
-//
+//RSI
 const rsi_async = promisify(tulind.indicators.rsi.indicator);
+//MACD
 const macd_async = promisify(tulind.indicators.macd.indicator);
 //ROC
 const roc_async = promisify(tulind.indicators.roc.indicator);
@@ -29,46 +32,25 @@ const obv_async = promisify(tulind.indicators.obv.indicator);
 const sma_inc = async (data) => {
 	console.log(tulind.indicators.sma);
 	// const d1 = data.map((d) => d.close);
-	const results = await sma_async([data], [3]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, sma: d3[i] }));
-	
+	const results = await sma_async([data], [3]);	
 	return results;
 };
 
 const ema_inc = async (data) => {
 	// const d1 = data.map((d) => d.close);
 	const results = await ema_async([data], [3]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
 const bbands_inc = async (data) => {
 	// const d1 = data.map((d) => d.close);
 	const results = await bbands_async([data], [3,2]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
 const wma_inc = async (data) => {
 	// const d1 = data.map((d) => d.close);
 	const results = await wma_async([data], [2]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
@@ -77,22 +59,12 @@ const wma_inc = async (data) => {
 
 const rsi_inc = async (data) => {
 	const results = await rsi_async([data], [2]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
 //
 const macd_inc = async (data) => {
 	const results = await macd_async([data], [2,3,5]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
@@ -100,33 +72,18 @@ const macd_inc = async (data) => {
 const roc_inc = async (data) => {
 	// const d1 = data.map((d) => d.close);
 	const results = await roc_async([data], [2]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
 const stoch_inc = async (data1,data2,data3) => {
 	// const d1 = data.map((d) => d.close);
 	const results = await stoch_async([data1,data2,data3],[3,2,2]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
 const obv_inc = async (data1,data2) => {
 	// const d1 = data.map((d) => d.close);
 	const results = await obv_async([data1,data2],[]);
-	// const d2 = results[0];
-	// const diff = data.length - d2.length;
-	// const emptyArray = [...new Array(diff)].map((d) => '');
-	// const d3 = [...emptyArray, ...d2];
-	// data = data.map((d, i) => ({ ...d, ema: d3[i] }));
 	return results;
 };
 
